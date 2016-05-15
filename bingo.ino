1c19d5b39af62aa922c993d3f8e1c61b7a4e971d
David Lakata <davidlakata@gmail.com>
2016-05-15 01:27:46 -0400

    fleshed out more of the logic

--- bingo.ino
+++ bingo.ino
@@ -54,6 +54,9 @@ char BINGO[] = "BINGO";
 
 int rand_x, rand_y;
 
+int bingos_left = 50;
+int game_over = 0;
+
 void draw_square(int x, int y, int color) {
   tft.fillRect(x * BOXSIZE, BINGO_OFFSET + y * BOXSIZE, BOXSIZE, BOXSIZE, color);
   tft.drawRect(x * BOXSIZE, BINGO_OFFSET + y * BOXSIZE, BOXSIZE, BOXSIZE, WHITE);
@@ -62,6 +65,14 @@ void draw_square(int x, int y, int color) {
 void write_square(int x, int y, int num) {
   tft.setCursor(x * BOXSIZE + BOXSIZE / 2 - 8, BINGO_OFFSET + y * BOXSIZE + BOXSIZE / 2 - 8);
   tft.println(num);
+}
+
+int decrement_bingos() {
+  int decrement = random(0, 20);
+  if (decrement < 1) {
+    return 1; 
+  }
+  return 0;
 }
 
 int get_random_number() {
@@ -103,6 +114,39 @@ void reset_board() {
     }
   }
   get_random_square();
+  tft.fillRect(2 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
+  tft.fillRect(3 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
+  tft.setCursor(2 * BOXSIZE, 6 * BOXSIZE);
+  tft.println(BINGO[rand_x]);
+  tft.setCursor(3 * BOXSIZE, 6 * BOXSIZE);
+  tft.println(numbers[rand_x][rand_y]);
+
+  tft.setCursor(BOXSIZE / 4, 7 * BOXSIZE);
+  tft.println("RESET");
+}
+
+int victory(int x, int y) {
+  int row = 1;
+  int col = 1;
+  for (int i = 0; i < NUM_SQUARES; i++) {
+    if (numbers[x][i] != -1) {
+      col = 0; 
+    }
+    if (numbers[i][y] != -1) {
+      row = 0; 
+    }
+  }
+  if (row == 1) {
+    for (int i = 0; i < NUM_SQUARES; i++) {
+      draw_square(i, y, GREEN);
+    }
+  } 
+  else if (col == 1) {
+    for (int i = 0; i < NUM_SQUARES; i++) {
+      draw_square(x, i, GREEN);
+    }
+  }
+  return row | col;
 }
 
 void setup(void) {
@@ -121,21 +165,25 @@ void setup(void) {
 #define MINPRESSURE 100
 #define MAXPRESSURE 1000
 
-void determine_square(TSPoint p, int *box_x, int *box_y) {
+#define RESET -2
+#define NOT_A_SQUARE -1
+
+int determine_square(TSPoint p, int *box_x, int *box_y) {
   *box_x = p.x / BOXSIZE;
   *box_y = (p.y - BINGO_OFFSET) / BOXSIZE;
-  if (*box_y > NUM_SQUARES - 1 || *box_x > NUM_SQUARES - 1) {
-    *box_x = -1;
-    *box_y = -1;
-    return;
+  if (p.x < BOXSIZE && p.y > 6 * BOXSIZE) {
+    return RESET; 
   }
+  if (*box_y > NUM_SQUARES - 1 || *box_x > NUM_SQUARES - 1) {
+    *box_x = NOT_A_SQUARE;
+    *box_y = NOT_A_SQUARE;
+  }
+  return 0;
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
   TSPoint p = ts.getPoint();
-  digitalWrite(13, LOW);
 
   // if sharing pins, you'll need to fix the directions of the touchscreen pins
   //pinMode(XP, OUTPUT);
@@ -149,30 +197,45 @@ void loop()
     p.y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0);
 
     int x, y;
-    determine_square(p, &x, &y);
+    if (determine_square(p, &x, &y) == RESET) {
+      reset_board();
+      return;
+    }
+    if (game_over) {
+      return;
+    }
     if (x == rand_x && y == rand_y) {
       numbers[rand_x][rand_y] = -1;
       draw_square(x, y, RED);
       get_random_square();
+      tft.fillRect(2 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
+      tft.fillRect(3 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
+      tft.setCursor(2 * BOXSIZE, 6 * BOXSIZE);
+      tft.println(BINGO[rand_x]);
+      tft.setCursor(3 * BOXSIZE, 6 * BOXSIZE);
+      tft.println(numbers[rand_x][rand_y]);
     }
-    tft.fillRect(1 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
-    tft.setCursor(1 * BOXSIZE + BOXSIZE / 2 - 8, 6 * BOXSIZE + BOXSIZE / 2 - 8);
-    tft.println(x);
-    tft.fillRect(2 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
-    tft.setCursor(2 * BOXSIZE + BOXSIZE / 2 - 8, 6 * BOXSIZE + BOXSIZE / 2 - 8);
-    tft.println(y);
-    tft.fillRect(3 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
-    tft.setCursor(3 * BOXSIZE + BOXSIZE / 2 - 8, 6 * BOXSIZE + BOXSIZE / 2 - 8);
-    tft.println(BINGO[rand_x]);
-    tft.fillRect(4 * BOXSIZE, 6 * BOXSIZE, BOXSIZE, BOXSIZE, BLACK);
-    tft.setCursor(4 * BOXSIZE + BOXSIZE / 2 - 8, 6 * BOXSIZE + BOXSIZE / 2 - 8);
-    tft.println(numbers[rand_x][rand_y]);
+
+    if (bingos_left <= 0) {
+      tft.fillRect(2 * BOXSIZE, 7 * BOXSIZE, 3 * BOXSIZE, BOXSIZE, BLACK);
+      tft.setCursor(3 * BOXSIZE, 7 * BOXSIZE);
+      tft.println("Game over!");
+      game_over = 1;
+    } 
+    else if (decrement_bingos()) {
+      bingos_left--;
+      tft.fillRect(2 * BOXSIZE, 7 * BOXSIZE, 3 * BOXSIZE, BOXSIZE, BLACK);
+      tft.setCursor(2 * BOXSIZE, 7 * BOXSIZE);
+      tft.println("Bingos left:");
+      tft.setCursor(4 * BOXSIZE + BOXSIZE / 2, 7 * BOXSIZE);
+      tft.println(bingos_left);
+    }
+    if (victory(x, y)) {
+      tft.fillRect(2 * BOXSIZE, 7 * BOXSIZE, 3 * BOXSIZE, BOXSIZE, BLACK);
+      tft.setCursor(3 * BOXSIZE, 7 * BOXSIZE);
+      tft.println("You won!");
+      game_over = 1;
+    }
   }
 }
-
-
-
-
-
-
 
